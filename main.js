6497eb30d981de50280c8b26f6a67f21c144784e
Mapuremba <98226946+Mapuremba@users.noreply.github.com>
2022-07-18 18:55:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+ function CanvasReference
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	var img_imgTag = new Image(); 
+function img_imgTag.onload = uploadimg; 
+	 img_imgTag.src = load image;   
+}
+
+function uploading() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+function keyPressedEvent
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		function check KeyPressed
+	else{
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	}
+}
+
+function aplhabetkey()
+{
+	img_image="aplhabetkey.jpg";
+    add();
+
+}
+function numberkey()
+{
+	img_image="numberkey.jpg";
+    add();
+}
+function arrowkey()
+{
+    img_image="numberkey.jpg";
+    add();
+}
+function specialkey()
+{
+	img_image="specialkey.jpg";
+    add();
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
